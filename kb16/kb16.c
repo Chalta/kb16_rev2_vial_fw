3fb27ad9eecccae7bf05ebcb86cec8a68fced62f
Chalta <bills@fiss.ca>
2024-06-05 15:50:29 -0600

    Add files via upload

--- kb16/kb16.c
+++ kb16/kb16.c
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "kb16.h"
+#include "quantum.h"
 
 // OLED animation
 #include "./lib/logo.h"
@@ -36,12 +36,29 @@
             render_logo();
         } else {
             finished_logo = true;
-			
             if (!oled_task_user()) {
                 return false;
             }
         }
-
         return true;
     }
+#endif
+
+#ifdef RGB_MATRIX_ENABLE
+led_config_t g_led_config = { {
+    {  0,      1,      2,      3 },
+    {  4,      5,      6,      7 },
+    {  8,      9,     10,     11 },
+    { 12,     13,     14,     15 }
+}, {
+    {0  ,  0}, {75 ,  0}, {149,  0}, {224,  0},
+    {0  , 21}, {75 , 21}, {149, 21}, {224, 21},
+    {0  , 43}, {75 , 43}, {149, 43}, {224, 43},
+    {0  , 64}, {75 , 64}, {149, 64}, {224, 64},
+}, {
+    1, 1, 1, 1,
+    1, 4, 4, 1,
+    1, 4, 4, 1,
+    1, 1, 1, 1,
+} };
 #endif
